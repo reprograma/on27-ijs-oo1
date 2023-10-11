37b44ef266ed3c3fbe9fc3496f7dbac9ddf4f14d
Beatriz Locatelli <beatrizglocatelli@gmail.com>
2023-10-11 17:33:45 -0300

    Resolução do exercício da semana 4 - POO - SuperHerois

---
+++ exercicios/para-casa/entregas/beatriz-locatelli/Classes/Confronto.js
@@ -0,0 +1,28 @@
+import { Personagem } from "./Personagem.js";
+
+export class Confronto {
+
+    lutar(personagem1, personagem2) {
+        const personagem1PoderTotal = personagem1.poderTotal()
+        const personagem2PoderTotal = personagem2.poderTotal()
+
+        if (!(personagem1 && personagem1 instanceof Personagem)) {
+            console.log("Personagens não econtrados.")
+        }
+        if (personagem1PoderTotal > personagem2PoderTotal) {
+            console.log(`Resultado do confronto: O personagem ${personagem1.nome} venceu!`)
+            return 1
+
+        } else if (personagem1PoderTotal < personagem2PoderTotal) {
+            console.log(`Resultado do confronto: O personagem ${personagem2.nome} venceu!`)
+            return 2
+            
+        } else {
+            personagem1PoderTotal === personagem2PoderTotal
+            console.log(`Resultado do confronto: empate!`)
+            return 0
+        }
+    }
+}
+
+
