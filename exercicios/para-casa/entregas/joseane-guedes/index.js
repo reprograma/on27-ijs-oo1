3b1cc49f679276b8de19e42f034b56550dfeeace
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-10-12 18:20:30 -0300

    Add testes

--- exercicios/para-casa/entregas/joseane-guedes/index.js
+++ exercicios/para-casa/entregas/joseane-guedes/index.js
@@ -1,15 +1,21 @@
 import { SuperPoder } from './SuperPoder.js';
+import { Personagem } from './Personagem.js';
+
+const superPoder1 = new SuperPoder('Coragem', 3);
+
+const personagem1 = new Personagem('Mulher do Brasil', 'Maria');
+personagem1.adicionaSuperPoder(superPoder1);
+personagem1.poderTotal();
+
+const poderTotal = personagem1.poderTotal();
 
 
-const superPoder1 = new SuperPoder('invisivel', 3);
-const superPoder2 = new SuperPoder('coragem', 3);
+console.log(`O poder total é ${poderTotal}`);
 
+console.log(`O nome do personagem é ${personagem1.nome} e na vida real ela se chama ${personagem1.nomeVidaReal} e tem o poder total ${personagem1.poderTotal()} `)
 
 console.log(`O poder é ${superPoder1.nome} e a categoria do poder é ${superPoder1.categoria}`);
 
-
-
-
-
+console.log(`O super poder é ${superPoder1.nome} para ${personagem1.nome}`)
 
 
