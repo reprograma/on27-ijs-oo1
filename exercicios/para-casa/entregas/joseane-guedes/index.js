1e837405e97b9a66e6e579d828419f698a986988
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-10-12 16:49:37 -0300

    Add SuperPoder e testes

---
+++ exercicios/para-casa/entregas/joseane-guedes/index.js
@@ -0,0 +1,15 @@
+import { SuperPoder } from './SuperPoder.js';
+
+
+const superPoder1 = new SuperPoder('invisivel', 3);
+const superPoder2 = new SuperPoder('coragem', 3);
+
+
+console.log(`O poder é ${superPoder1.nome} e a categoria do poder é ${superPoder1.categoria}`);
+
+
+
+
+
+
+
