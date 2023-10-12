c5803815f45d950b7cdb067c0c95882be754cd7e
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-10-12 19:43:39 -0300

    Add confronto e testes

--- exercicios/para-casa/entregas/joseane-guedes/index.js
+++ exercicios/para-casa/entregas/joseane-guedes/index.js
@@ -1,21 +1,27 @@
 import { SuperPoder } from './SuperPoder.js';
 import { Personagem } from './Personagem.js';
+import { Confronto } from './Confronto.js';
 
 const superPoder1 = new SuperPoder('Coragem', 3);
 
-const personagem1 = new Personagem('Mulher do Brasil', 'Maria');
-personagem1.adicionaSuperPoder(superPoder1);
-personagem1.poderTotal();
 
-const poderTotal = personagem1.poderTotal();
+const superheroi = new Personagem('Mulher do Brasil', 'Maria');
+superheroi.adicionaSuperPoder(superPoder1);
+superheroi.poderTotal();
 
+const poderTotal = superheroi.poderTotal();
 
 console.log(`O poder total é ${poderTotal}`);
 
-console.log(`O nome do personagem é ${personagem1.nome} e na vida real ela se chama ${personagem1.nomeVidaReal} e tem o poder total ${personagem1.poderTotal()} `)
+
+console.log(`O nome do personagem é ${superheroi.nome} e na vida real ela se chama ${superheroi.nomeVidaReal} e tem o poder total ${superheroi.poderTotal()} `)
 
 console.log(`O poder é ${superPoder1.nome} e a categoria do poder é ${superPoder1.categoria}`);
 
-console.log(`O super poder é ${superPoder1.nome} para ${personagem1.nome}`)
+console.log(`O super poder é ${superPoder1.nome} para ${superheroi.nome}`)
+
+
+const confrontos = new Confronto();
+console.log(`${confrontos.lutar}`)
 
 
